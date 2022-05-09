202ee25e439b0eb06b3a2124572289bb0cb422d0
Unimar <Aula@maq04.lab421>
2022-05-09 20:55:57 -0300

    Adição das rotas insert, delete e update em documentos

--- src/features/model/documents.ts
+++ src/features/model/documents.ts
@@ -14,5 +14,46 @@ async function listAllDocuments(){
     }
 }
 
+async function insertDocuments(){
 
-module.exports = {listAllDocuments}
+    const sql = `insert`
+
+    try {
+
+        const res = await documentsConnection.client.query(sql);
+        return res.rows;
+
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+async function deleteDocuments(documents_id: number){
+    const sql = `delete from public.documents where documents.documents_id = $1`
+    const values = [documents_id]
+
+    try{
+        const res = await documentsConnection.client.query(sql, values);
+        return res.rows;
+    }catch (err){
+        return(err);
+    }
+}
+
+async function updateDocuments(documents_id: number, value1: string, value2: string){
+
+    const sql = `UPDATE`
+    const values = [value1, value2, documents_id]
+
+    try {
+
+        const res = await documentsConnection.client.query(sql, values);
+        return res.rows;
+
+    } catch (err) {
+        return(err);
+    }
+}
+
+
+module.exports = {listAllDocuments, insertDocuments, deleteDocuments, updateDocuments}
